1028e02f597b8e75fb760e80155564fcb0fab3e6
Lucas Lacerda <lucaslacerdacl@gmail.com>
2022-11-02 12:22:29 -0300

    #deploy

--- core/publish-s3-bucket.sh
+++ core/publish-s3-bucket.sh
@@ -5,5 +5,5 @@ if [ "$PUBLIC_ACL" == "true" ]; then
 fi
 
 aws s3 sync ./${BUILD_FOLDER} s3://${BUCKET_NAME} \
-  --storage-class=INTELLIGENT_TIERING
+  --storage-class=INTELLIGENT_TIERING \
   --acl $ACL
