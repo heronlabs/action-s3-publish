2b2d0aebff56bcbe14ba7a6686616098c4e7ad59
Lucas Lacerda <lucaslacerdacl@gmail.com>
2022-11-02 12:15:56 -0300

    #deploy

--- core/publish-s3-bucket.sh
+++ core/publish-s3-bucket.sh
@@ -1,2 +1,9 @@
+ACL="private";
+
+if [ "$PUBLIC_ACL" == "true" ]; then
+  ACL="public-read"
+fi
+
 aws s3 sync ./${BUILD_FOLDER} s3://${BUCKET_NAME} \
   --storage-class=INTELLIGENT_TIERING
+  --acl $getAcl
